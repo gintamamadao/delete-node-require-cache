15edb89eeb1ceb0d585d4fbe2ed5ef03a107f066
gintamamadao <gintamaginsan@126.com>
2021-11-28 21:03:48 +0800

    feat: del require cache

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { resolve, dirname, extname } from 'path'
 import fsUtil from 'ginlibs-file-util'
 import validateNpm from 'validate-npm-package-name'
-import recast from 'recast'
+import parser from '@babel/parser'
 
 export const delRequireCache = async (rootFile: string) => {
   const getAllDependJS = (file: string) => {
@@ -9,29 +9,14 @@ export const delRequireCache = async (rootFile: string) => {
       extname(file) === '.js' || extname(file) === '.jsx' ? file : `${file}.js`
     const result = [jsFile]
     const jsCont = fsUtil.read(jsFile)
-    const jsAst = recast.parse(jsCont)
-    recast.types.visit(jsAst, {
-      visitCallExpression(path: any) {
-        const calleeName = path?.value.callee?.name
-        if (calleeName !== 'require') {
-          return false
-        }
-        const arg = path.value?.arguments?.[0]?.value
-        if (validateNpm(arg)?.validForNewPackages) {
-          return false
-        }
-        const itJsFile = resolve(dirname(file), arg)
-
-        Array.prototype.push.apply(result, getAllDependJS(itJsFile) || [])
-        return false
-      },
-    })
+    const jsAst = parser.parse(jsCont)
+    console.log(jsAst, 'jsAst89078')
     return result
   }
+  getAllDependJS(rootFile)
+  // const allDepJS = getAllDependJS(rootFile)
 
-  const allDepJS = getAllDependJS(rootFile)
-
-  for (const itJS of allDepJS) {
-    delete require.cache[require.resolve(itJS)]
-  }
+  // for (const itJS of allDepJS) {
+  //   delete require.cache[require.resolve(itJS)]
+  // }
 }
