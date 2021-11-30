9aa5cf61938fb2cf952e31c6ac9d9019c4275797
gintamamadao <gintamaginsan@126.com>
2021-11-30 22:00:27 +0800

    feat: change to babel

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,71 @@ import { resolve, dirname, extname } from 'path'
 import fsUtil from 'ginlibs-file-util'
 import validateNpm from 'validate-npm-package-name'
 import parser from '@babel/parser'
+import traverse from '@babel/traverse'
 
-export const delRequireCache = async (rootFile: string) => {
-  const getAllDependJS = (file: string) => {
-    const jsFile =
-      extname(file) === '.js' || extname(file) === '.jsx' ? file : `${file}.js`
-    const result = [jsFile]
-    const jsCont = fsUtil.read(jsFile)
-    const jsAst = parser.parse(jsCont)
-    console.log(jsAst, 'jsAst89078')
+export const delCache = async (rootFile: string) => {
+  const getAllDependJS = (filePath: string) => {
+    const jsCont = fsUtil.read(filePath)
+    const result = [
+      filePath.slice(0, filePath.length - extname(filePath).length),
+    ]
+    const jsAst = parser.parse(jsCont, {
+      sourceType: 'module',
+      plugins: ['typescript', 'jsx'],
+    })
+    traverse(jsAst, {
+      ImportDeclaration(path) {
+        const sourceVal = path?.node?.source?.value
+        const itFile = resolve(dirname(filePath), sourceVal)
+        if (validateNpm(sourceVal)?.validForNewPackages || !sourceVal) {
+          return
+        }
+        Array.prototype.push.apply(result, getAllDependJS(itFile) || [])
+      },
+      VariableDeclarator(path: any) {
+        traverse(
+          path.node,
+          {
+            CallExpression(cPath: any) {
+              const name = cPath?.node?.callee?.name
+              if (name !== 'require') {
+                return
+              }
+              traverse(
+                cPath.node,
+                {
+                  StringLiteral(csPath: any) {
+                    const csValue = csPath?.node?.value
+                    const itFile = resolve(dirname(filePath), csValue)
+                    if (csValue) {
+                    }
+                    if (validateNpm(csValue)?.validForNewPackages || !csValue) {
+                      return
+                    }
+                    Array.prototype.push.apply(
+                      result,
+                      getAllDependJS(itFile) || []
+                    )
+                  },
+                },
+                path.scope,
+                path.state,
+                path.parentPath
+              )
+            },
+          },
+          path.scope,
+          path.state,
+          path.parentPath
+        )
+      },
+    })
     return result
   }
-  getAllDependJS(rootFile)
-  // const allDepJS = getAllDependJS(rootFile)
-
-  // for (const itJS of allDepJS) {
-  //   delete require.cache[require.resolve(itJS)]
-  // }
+  const allDepJS = getAllDependJS(rootFile)
+  for (const itJS of allDepJS) {
+    if (fsUtil.exist(`${itJS}.js`)) {
+      delete require.cache[require.resolve(itJS)]
+    }
+  }
 }
