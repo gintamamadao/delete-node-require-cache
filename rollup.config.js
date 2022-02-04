392f7ae60005613ef5438c5a81f77634c64386eb
gintamamadao <gintamaginsan@126.com>
2022-02-04 19:54:34 +0800

    feat: update pkg

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,33 @@
-import typescript from "rollup-plugin-typescript2";
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import babel from "@rollup/plugin-babel";
-import pkg from "./package.json";
-import json from "@rollup/plugin-json";
-import { preserveShebangs } from "rollup-plugin-preserve-shebangs";
-import externals from "rollup-plugin-node-externals";
-import shelljs from 'shelljs'
-
-const extensions = [".js", ".ts"];
+import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
+import pkg from './package.json'
+import json from '@rollup/plugin-json'
+import { preserveShebangs } from 'rollup-plugin-preserve-shebangs'
+import externals from 'rollup-plugin-node-externals'
 
 const babelOpt = {
-  extensions,
-  babelHelpers: "runtime",
-  exclude: "node_modules/**",
-  include: ["src/**/*"],
-  plugins: ["@babel/plugin-transform-runtime"],
+  extensions: ['.js', '.ts'],
+  babelHelpers: 'runtime',
+  exclude: 'node_modules/**',
+  include: ['src/**/*'],
+  plugins: ['@babel/plugin-transform-runtime'],
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         targets: {
-          node: "10",
+          node: '10',
         },
       },
     ],
   ],
-};
+}
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     external: Object.keys(pkg.dependencies),
     output: [{ file: pkg.main, format: 'cjs' }],
     plugins: [
@@ -47,12 +44,6 @@ export default [
       resolve(),
       commonjs(),
       babel(babelOpt),
-      {
-        name: 'cpTpl',
-        writeBundle() {
-          shelljs.cp('-R', 'src/tpl', 'bin/')
-        },
-      },
     ],
   },
-];
+]
